90c3536dad068893f8d97e16226350d830ef0155
João Paulo - Acer <joaopaulosil@yahoo.com.br>
2018-07-16 14:05:29 -0300

    adicionada uma pagina nova

---
+++ Estacionamento2/conteudo_incio.php
@@ -0,0 +1,13 @@
+<?php
+	date_default_timezone_set('America/Sao_Paulo');
+	
+	require_once('db.class.php');
+	
+?>
+<div id="content"> 
+	conteudo_incio															
+</div><!-- /conteudo -->
+
+	<script src="assets/js/scripts.js"></script>
+
+	
